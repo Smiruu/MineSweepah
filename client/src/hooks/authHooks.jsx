ab41f8784a942fdc8ae1b19acb45c0ccd6eb911e
Smiruu <justineangelodevera@gmail.com>
2025-08-24 21:39:35 +0800

    Added deployment stuff

--- client/src/hooks/authHooks.jsx
+++ client/src/hooks/authHooks.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/auth",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const useAuth = () => {
