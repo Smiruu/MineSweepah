ce6646d0d72ea8ed410bb135c6aaa820714cde57
Smiruu <justineangelodevera@gmail.com>
2025-09-09 19:00:50 +0800

    chore: cleaned save highscore api call

--- client/src/hooks/scoreHooks.jsx
+++ client/src/hooks/scoreHooks.jsx
@@ -1,44 +1,23 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-export const useSubmitScore = (time, gameStatus, difficulty = "easy") => {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [submitted, setSubmitted] = useState(false);
-
-  useEffect(() => {
-    if (gameStatus !== "won" || submitted) return; // Only submit on win
-
-    const submitScore = async () => {
-      setLoading(true);
-      setError(null);
-
-      try {
-        const token = localStorage.getItem("access_token");
-        const res = await API.post(
-          "/api/scores/highscore",
-          { time, difficulty },
-          { headers: { Authorization: `Bearer ${token}` } }
-        );
-
-        console.log("Score submitted:", res.data);
-        setSubmitted(true);
-      } catch (err) {
-        console.error("Error submitting score:", err);
-        setError(err.response?.data?.error || "Failed to submit score");
-      } finally {
-        setLoading(false);
+export const useSubmitScore = async(time, gameStatus, difficulty ) => {
+  try {
+    const token = localStorage.getItem("access_token")
+    await API.post("/api/scores/highscore",
+      {
+        params: {time, gameStatus, difficulty},
+        headers: {Authorization: `Bearer ${token}`}
       }
-    };
+    )
 
-    submitScore();
-  }, [time, gameStatus, difficulty, submitted]);
-
-  return { loading, error, submitted };
+  } catch (err) {
+    throw new Error(err?.response?.data?.error || "Failed to set High Score");
+  }
 };
 
 export const getHighScore = async(difficulty) => {
